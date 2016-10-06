fd064299ede60a2a562de080ff69c865bcf98294
Mark Servilla <mark.servilla@gmail.com>
2016-10-05 20:33:41 -0600

    Change hash from MD5 to SHA1; flush output buffer per line

--- dircheck.py
+++ dircheck.py
@@ -25,12 +25,12 @@ logger = logging.getLogger('dircheck')
 import os
 from os.path import join, getsize
 import sys
-from hashlib import md5
+from hashlib import sha1
 import getopt
 
 
-def md5sum(file_name):
-    hash = md5()
+def sha1sum(file_name):
+    hash = sha1()
     with open(file_name, "rb") as f:
         for chunk in iter(lambda: f.read(128 * hash.block_size), b""):
             hash.update(chunk)
@@ -41,7 +41,7 @@ def main(argv):
 
     synopsis = '"dircheck" will recursively iterate through a directory ' + \
                'tree and list the name for each file and, if verbosity ' + \
-               'is requested, the full path, size, and MD5 checksum hash ' + \
+               'is requested, the full path, size, and SHA1 checksum hash ' + \
                'for each file.'
 
     usage = 'Usage: python ./dircheck.py -h (help) | ' + \
@@ -87,15 +87,16 @@ def main(argv):
             for file_name in files:
                 file_path = join(root,file_name)
                 file_size =  getsize(file_path)
-                file_hash = md5sum(file_path)
+                file_hash = sha1sum(file_path)
 
                 print('{0}, {1}, {2}, {3}'.format(file_name, file_path,
                                                   file_size, file_hash),
                       file = out_file)
+                out_file.flush()
         else:
             for file_name in files:
                 print('{0}'.format(file_name), file = out_file)
-
+                out_file.flush()
 
 
 if __name__ == "__main__":
