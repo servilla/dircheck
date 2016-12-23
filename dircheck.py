fd1d4c5a224eb6849289c0fa9cdbf1bfbfa4f72c
Mark Servilla <mark.servilla@gmail.com>
2016-12-22 19:26:02 -0700

    Add greater fidelity to CLI options

--- dircheck.py
+++ dircheck.py
@@ -39,26 +39,29 @@ def sha1sum(file_name):
 
 def main(argv):
 
-    synopsis = '"dircheck" will recursively iterate through a directory ' + \
-               'tree and list the name for each file and, if verbosity ' + \
-               'is requested, the full path, size, and SHA1 checksum hash ' + \
-               'for each file.'
+    synopsis = '"dircheck.py" will recursively iterate through a directory ' \
+               'tree and list the name for each file and, if requested, ' \
+               'the full path, size, and SHA1 checksum hash for each file.'
 
-    usage = 'Usage: python ./dircheck.py -h (help) | ' + \
-            '[-v verbosity] [-o output_file] <root_directory_path>'
+    usage = 'Usage: python ./dircheck.py -h (help) | [-p no file path] ' \
+            '[-s no file size] [-c no checksum] [-o output_file] ' \
+            '<directory_path>'
 
     if len(argv) == 0:
         logger.error(usage)
         sys.exit(1)
 
     try:
-        opts, args = getopt.getopt(argv, 'hvo:')
+        opts, args = getopt.getopt(argv, 'hpsco:')
     except getopt.GetoptError as e:
         logger.error('Unrecognized command line flag: {0}'.format(e))
         logger.error(usage)
         sys.exit(1)
 
-    verbosity = False
+    _file_path = False
+    _file_size = False
+    _checksum = False
+
     out_file = sys.stdout
 
     for opt, arg in opts:
@@ -66,8 +69,12 @@ def main(argv):
             print(synopsis)
             print(usage)
             sys.exit(0)
-        elif opt == '-v':
-            verbosity = True
+        elif opt == '-p':
+            _file_path = True
+        elif opt == '-s':
+            _file_size = True
+        elif opt == '-c':
+            _checksum = True
         elif opt == '-o':
             out_file = open(arg, 'w')
         else:
@@ -83,23 +90,23 @@ def main(argv):
 
     for root, dirs, files in os.walk(dir_path):
 
-        if verbosity:
-            for file_name in files:
-                file_path = join(root,file_name)
-                file_size =  getsize(file_path)
+        for file_name in files:
+            file_path = join(root,file_name)
+            file_size = ''
+            checksum = ''
+            if _file_path:
+                file_name = file_path
+            if _file_size:
+                file_size = ', ' + str(getsize(file_path))
+            if _checksum:
                 try:
-                    file_hash = sha1sum(file_path)
+                    checksum = ', ' + sha1sum(file_path)
                 except IOError as e:
                     logger.error(e)
 
-                print('{0}, {1}, {2}, {3}'.format(file_name, file_path,
-                                                  file_size, file_hash),
-                      file = out_file)
-                out_file.flush()
-        else:
-            for file_name in files:
-                print('{0}'.format(file_name), file = out_file)
-                out_file.flush()
+            print('{0}{1}{2}'.format(file_name, file_size, checksum),
+                  file = out_file)
+            out_file.flush()
 
 
 if __name__ == "__main__":
